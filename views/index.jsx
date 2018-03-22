ccf00d3ea2528d1f0da2be446bd1c647e4cd9ae5
Rahsaan Basek <rahsaanbasek@gmail.com>
2018-03-21 22:53:36 -0400

    maked react server

---
+++ views/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+    
+    export default class TodoBox extends React.Component{
+      render() {
+        return <div className="todoBox">
+            Hello, world!
+          </div>
+      }
+    }
+    
