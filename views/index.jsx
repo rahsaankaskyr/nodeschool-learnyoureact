17cf37ed64df945c64da0aae94c6aa265080960e
Rahsaan Basek <rahsaanbasek@gmail.com>
2018-04-06 11:27:28 -0400

    react components

--- views/index.jsx
+++ views/index.jsx
@@ -1,3 +1,5 @@
+/* 
+
 import React from 'react';
     
     export default class TodoBox extends React.Component{
@@ -7,4 +9,46 @@ import React from 'react';
           </div>
       }
     }
+
+    */
+
+   import React from 'react';
     
+   export default class TodoBox extends React.Component {
+       render() {
+           return (
+               <div className="todoBox">
+                   <h1>Todos</h1>
+                   <TodoList />
+                   <TodoForm />
+               </div>
+           );
+       }
+   }
+   
+   class TodoList extends React.Component {
+    render() {
+      return <<div className="todoList">
+      I am a TodoList.
+      </div>
+   }
+   }
+   
+   class TodoForm extends React.Component {
+    render() {
+      return class TodoForm extends React.Component {
+      <div className="todoForm">
+      I am a TodoForm.
+      </div>
+     }
+   }
+   
+  
+  
+   
+  
+
+   
+ // export default class MyComponent extends React.Component {/*...*/};
+ //   let myElement = <MyComponent someProperty={true} />;
+ //   ReactDOM.render(myElement, document.getElementById('example'));
